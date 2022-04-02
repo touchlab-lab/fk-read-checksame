bfc5d5cc2ed95c999571ef82dcd7986988a00573
Kevin Galligan <kgalligan@gmail.com>
2022-04-02 18:28:02 -0400

    Read all checksums

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 // import * as github from '@actions/github'
+import * as path from 'path'
 import {existsSync, promises as fsp} from 'fs'
 
 interface Checkdiff {
@@ -8,23 +9,49 @@ interface Checkdiff {
 
 async function run(): Promise<void> {
   try {
-    const modulePath: string = core.getInput('faktory_module')
-    core.debug(`Reading from ${modulePath}`)
-
-    const checkdiffPath = `${modulePath}/.faktory/checkdiff`
-    if (existsSync(checkdiffPath)) {
-      const fileData = await fsp.readFile(
-        `${modulePath}/.faktory/checkdiff`,
-        'utf8'
+    const allDiffs = await walkDir('.')
+    const bigDiff = allDiffs
+      .sort((a: CheckdiffInfo, b: CheckdiffInfo) =>
+        a.basePath < b.basePath ? -1 : b.basePath < a.basePath ? 1 : 0
       )
+      .map(value => value.checkdiff)
+      .join('|')
 
-      const fkdata: Checkdiff = JSON.parse(fileData)
-      core.setOutput('checksum', fkdata.checksum)
-    } else {
-      core.setOutput('checksum', '')
-    }
+    core.debug(`bigDiff ${bigDiff}`)
+    core.setOutput('checksum', bigDiff)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+async function walkDir(basePath: string): Promise<CheckdiffInfo[]> {
+  const files = await fsp.readdir(basePath, {withFileTypes: true})
+  const allDiffs: CheckdiffInfo[] = []
+  for (const file of files) {
+    if (file.isDirectory()) {
+      if (file.name === '.faktory') {
+        const checkdiffPath = `${path.join(basePath, file.name, 'checkdiff')}`
+        if (existsSync(checkdiffPath)) {
+          const fileData = await fsp.readFile(checkdiffPath, 'utf8')
+          const fkdata: Checkdiff = JSON.parse(fileData)
+          return [new CheckdiffInfo(basePath, fkdata.checksum)]
+        }
+      } else {
+        const otherDiffs = await walkDir(path.join(basePath, file.name))
+        allDiffs.push(...otherDiffs)
+      }
+    }
+  }
+
+  return allDiffs
+}
+
+class CheckdiffInfo {
+  basePath: string
+  checkdiff: string
+  constructor(basePath: string, checkdiff: string) {
+    this.basePath = basePath
+    this.checkdiff = checkdiff
   }
 }
 
